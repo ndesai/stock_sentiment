b7b49f8f1a4b01c24d2cbfb2a47ab05cd446c69a
Niraj Desai <niraj@app.st>
2026-01-26 21:06:30 -0800

    fix path

--- preparer.py
+++ preparer.py
@@ -3,16 +3,17 @@
 import schema
 from jinja2 import Template
 import json
+from pathlib import Path
 
 
 def get_html(date: str, data: schema.StockAnalysisList) -> str:
-    with open("tpl/email.tpl.html", "r") as f:
+    with open(Path(__file__).parent / "tpl/email.tpl.html", "r") as f:
         template = Template(f.read())
     return template.render(date=date, stocks=data.stocks)
 
 
 if __name__ == "__main__":
-    with open("example/StockAnalysisList.json", "r") as f:
+    with open(Path(__file__).parent / "example/StockAnalysisList.json", "r") as f:
         data = schema.StockAnalysisList(**json.load(f))
     html = get_html("January 27, 2026 at 12:30:04", data)
     with open("result.html", "w") as f:
